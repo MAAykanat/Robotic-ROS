19ff748dd96614a78701a1a85a23f6526958bf8d
MAAykanat <m.a.aykanat@gmail.com>
2022-09-01 12:01:57 +0300

    Odom callback added

--- path-planning/trajectory_rollout.py
+++ path-planning/trajectory_rollout.py
@@ -21,22 +21,29 @@ class TurtleBot:
         self.rate = rospy.Rate(20) # Rate in Hz
 
         self.pose_gazebo = np.zeros((3,1), dtype=float)
+        self.pose_odom = np.zeros((3,1), dtype=float)
+
+        self.headingAngle_gazebo=None
+        self.headingAngle_odom=None
+
         self.gazebo_publisher = Twist()
+        self.odom_publisher = Twist()
     
     ###Subscribers###
     
     #-Gazebo-#
         self.subGazStates = rospy.Subscriber('/gazebo/model_states', ModelStates, self.cbGazStates, queue_size=10)
         #-Laser-#
-        self.subLaserScan = rospy.Subscriber('/scan', LaserScan, self.cbLaserScan, queue_size=10)
+        ##self.subLaserScan = rospy.Subscriber('/scan', LaserScan, self.cbLaserScan, queue_size=10)
         #-Imu-#
-        self.subImu = rospy.Subscriber('/imu', Imu, self.cbImu, queue_size=10)
+        ##self.subImu = rospy.Subscriber('/imu', Imu, self.cbImu, queue_size=10)
         #-Odometry-#
         self.subOdometry = rospy.Subscriber('/odom', Odometry, self.cbOdometry, queue_size=10)
         
         ###Publishers###
         self.pubVel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.gazebo_pub = rospy.Publisher('/gazebo_pub', Twist, queue_size=1)
+        self.gazebo_pub = rospy.Publisher('/gazebo_pub', Twist, queue_size=10)
+        self.odom_pub = rospy.Publisher('/odom_pub', Twist, queue_size=10)
     
     def cbGazStates(self,msg):
         if not msg == None:
@@ -56,25 +63,36 @@ class TurtleBot:
                     self.gazebo_publisher.linear.y = self.pose_gazebo[1]
                     self.gazebo_publisher.angular.z = self.headingAngle_gazebo
 
-                    self.gazebo_pub.publish(self.gazebo_publisher)
-
+                    #self.gazebo_pub.publish(self.gazebo_publisher)
                     print("Turtle-bot x position: {0}\nTurtle-bot y position: {1}\nTurtle-bot heading: {2}\n".format(self.pose_gazebo[0], self.pose_gazebo[1],self.headingAngle_gazebo))
+    #def cbLaserScan():
+    #    pass
     
-    def cbLaserScan():
-        pass
+    #def cbImu():
+    #    pass
     
-    def cbImu():
-        pass
+    def cbOdometry(self, msg):
+        self.pose_odom[0] = msg.pose.pose.position.x
+        self.pose_odom[1] = msg.pose.pose.position.y 
+        self.pose_odom[2] = msg.pose.pose.position.z
 
-    def cbOdometry():
-        pass
+        orientation_quarter = (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w)
+        euler = tf.transformations.euler_from_quaternion(orientation_quarter)
 
+        self.headingAngle_odom = euler[2]
 
+        self.odom_publisher.linear.x = self.pose_odom
+        self.odom_publisher.linear.y = self.pose_odom
+        self.odom_publisher.angular.z = self.headingAngle_odom
+        self.counter2 = self.counter2 +1 
+        print("Odom x position: {0}\nOdom y position: {1}\nOdom heading: {2}\n".format(self.pose_odom[0], self.pose_odom[1],self.headingAngle_odom))        
 
 def main():
     rospy.init_node('robot_node', anonymous=True)
     robot = TurtleBot()
     while not rospy.is_shutdown():
+        robot.gazebo_pub.publish(robot.gazebo_publisher)
+        robot.odom_pub.publish(robot.odom_publisher)
         robot.rate.sleep()
 
 if __name__ == '__main__':
