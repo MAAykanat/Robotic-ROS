1eb618081138af0fe38260a8116ea71a36d651f0
MAAykanat <m.a.aykanat@gmail.com>
2022-11-05 20:13:39 +0300

    Last work updated

--- path-planning/trajectory_rollout.py
+++ path-planning/trajectory_rollout.py
@@ -32,10 +32,12 @@ class TurtleBot:
 
         self.gazebo_publisher = Twist()
         self.odom_publisher = Twist()
+        self.motion_model_publisher = Twist()
+
     
     ###Subscribers###
     
-    #-Gazebo-#
+        #-Gazebo-#
         self.subGazStates = rospy.Subscriber('/gazebo/model_states', ModelStates, self.cbGazStates, queue_size=10)
         #-Laser-#
         self.subLaserScan = rospy.Subscriber('/scan', LaserScan, self.cbLaserScan, queue_size=10)
@@ -48,6 +50,8 @@ class TurtleBot:
         self.pubVel = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.gazebo_pub = rospy.Publisher('/gazebo_pub', Twist, queue_size=10)
         self.odom_pub = rospy.Publisher('/odom_pub', Twist, queue_size=10)
+        self.motion_model_pub = rospy.Publisher('/motion_model', Twist, queue_size=1) # Created to publish motion model value in same frequency as /cmd_vel
+
     
     def cbGazStates(self,msg):
         # Takes robot x,y and theta  from gazebo space
@@ -73,8 +77,8 @@ class TurtleBot:
     def cbLaserScan(self, msg):
         # Takes lidar data 
         self.laser_ranges = msg.ranges
-        print("-----Lidar reading--------")
-        print("Number of lidar message: ", len(self.laser_ranges))
+        # print("-----Lidar reading--------")
+        # print("Number of lidar message: ", len(self.laser_ranges))
     
     def cbImu(self, msg):
         # Imu sensor data
@@ -86,14 +90,14 @@ class TurtleBot:
         self.linear_acceleration[1]= msg.linear_acceleration.y
         self.linear_acceleration[2]= msg.linear_acceleration.z
 
-        print("-----Imu reading--------")
-        print("Angular Velocity x: ",self.angular_velocity[0])
-        print("Angular Velocity y: ",self.angular_velocity[1])
-        print("Angular Velocity z: ",self.angular_velocity[2])
-        print("------------------------")
-        print("Linear accelaration x: ",self.linear_acceleration[0])
-        print("Linear accelaration y: ",self.linear_acceleration[1])
-        print("Linear accelaration z: ",self.linear_acceleration[2])
+        # print("-----Imu reading--------")
+        # print("Angular Velocity x: ",self.angular_velocity[0])
+        # print("Angular Velocity y: ",self.angular_velocity[1])
+        # print("Angular Velocity z: ",self.angular_velocity[2])
+        # print("------------------------")
+        # print("Linear accelaration x: ",self.linear_acceleration[0])
+        # print("Linear accelaration y: ",self.linear_acceleration[1])
+        # print("Linear accelaration z: ",self.linear_acceleration[2])
     
     def cbOdometry(self, msg):
         # Takes robot x,y and theta from odometry message
@@ -111,12 +115,67 @@ class TurtleBot:
         self.odom_publisher.angular.z = self.headingAngle_odom
         # print("Odom x position: {0}\nOdom y position: {1}\nOdom heading: {2}\n".format(self.pose_odom[0], self.pose_odom[1],self.headingAngle_odom))        
 
+    def process_LIDAR(self):
+        if self.laser_ranges is not None:
+            # I have a 360x1 range vector
+            for i in range(0,len(self.laser_ranges)):
+                # Look for non-Inf data
+                if self.laser_ranges[i] is not np.inf:
+                    self.idx_bearing = i
+                    self.bearing = (i/180.)*np.pi
+                    self.dist = self.laser_ranges[i]
+                    # print('Detection dist:', self.dist, 'bearing:', self.bearing)
+                else:
+                    self.bearing = 99.
+                    self.dist = None
+                    # print('Object not detected by LIDAR')
+            # print("Done!!")
+
+    def move(self, linearx, lineary, angularz):
+        #Taking cmd_vel values and publish them
+        #Move Comment 
+        msg = Twist()
+        
+        self.linearx = linearx
+        self.lineary = lineary
+        self.angularz = math.radians(angularz)
+
+        msg.linear.x = linearx
+        msg.linear.y = lineary
+        msg.angular.z = angularz
+        self.pubVel.publish(msg)
+
+    def motion_model_(self):
+        #Motion Model of TurtleBot
+        self.motion_model_publisher.linear.x = (self.pose_odom[0] + (self.linearx*abs(math.cos(self.headingAngle_odom))))
+        self.motion_model_publisher.linear.y = (self.pose_odom[1] + (self.linearx*abs(math.sin(self.headingAngle_odom))))
+        self.motion_model_publisher.angular.z = (self.headingAngle_odom + (self.angular_velocity[2]*180/math.pi))
+
+        # self.linearx = (self.pose_odom[0] + (self.linearx*abs(math.cos(self.headingAngle_odom))))
+        # self.lineary = (self.pose_odom[1] + (self.linearx*abs(math.sin(self.headingAngle_odom))))
+        # self.angularz = (self.headingAngle_odom + (self.angular_velocity[2]*180/math.pi))
+        #In slide ME-597-3-Motion Modeling page 31, There is an motion model of TurtleBot
+        #[X1,t]                 [X1,t-1 + u1,t * cosX3,t-1 * dt]
+        #|X2,t| = g(Xt-1,ut) =  |X2,t-1 + u1,t * sinX3,t-1 * dt|
+        #[X3,t]                 [X3,t-1 + u2,t * dt]
+        #
+        print("Turtle bot x positon: {0} \nTurtle bot y positon: {1}\nTurtle bot heading: {2}\n\n\n".format(self.linearx, self.lineary, self.angularz))
+        self.motion_model_pub.publish(self.motion_model_publisher)
+
+
 def main():
     rospy.init_node('robot_node', anonymous=True)
     robot = TurtleBot()
+    
+    linearx = 0.1
+    lineary = 0.0
+    angularz = 0.04
+    
     while not rospy.is_shutdown():
-        robot.gazebo_pub.publish(robot.gazebo_publisher)
-        robot.odom_pub.publish(robot.odom_publisher)
+        #robot.gazebo_pub.publish(robot.gazebo_publisher)
+        robot.move(linearx, lineary, angularz) 
+        robot.motion_model_()
+        #robot.process_LIDAR()
         robot.rate.sleep()
 
 if __name__ == '__main__':
