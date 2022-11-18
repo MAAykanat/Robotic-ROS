4fdfbbb1a1d47b478a734f2305615f3d7e7ba803
MAAykanat <m.a.aykanat@gmail.com>
2022-11-18 11:44:25 +0300

    Direction keyboard defined works finegit add .!

--- keybord-control/keyboard_control.py
+++ keybord-control/keyboard_control.py
@@ -1,7 +1,26 @@
+#!/usr/bin/env python3
 import rospy
+import math
 from pynput import keyboard
 
 from nav_msgs import *
+from geometry_msgs.msg import *
+
+def move(linearx, lineary, angularz):
+    msg = Twist()
+    msg.linear.x = linearx
+    msg.linear.y = lineary
+    msg.angular.z = angularz
+    
+    pubVel.publish(msg)
+
+rospy.init_node('robot_node', anonymous=True)
+
+linearx = 0.0
+lineary = 0.0
+angularz = 0.0
+
+pubVel = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
 while(1):
     with keyboard.Events() as events:
@@ -9,13 +28,17 @@ while(1):
         event = events.get(0.05)
         if event is not None:
             if event.key == keyboard.KeyCode.from_char("w"):
+                move(0.3,0.0,0.0)
                 print("UP!!!")
             elif event.key == keyboard.KeyCode.from_char("d"):
+                move(0.0,0.0,-0.5)
                 print("RIGHT!!!")
             elif event.key == keyboard.KeyCode.from_char("a"):
+                move(0.0,0.0,0.5)
                 print("LEFT!!!")
             elif event.key == keyboard.KeyCode.from_char("s"):
                 print("DOWN!!!")
+                move(-0.3,0.0,0.0)
             elif event == None:
                 print("Stop")
             elif event.key == keyboard.Key.esc:
@@ -23,4 +46,5 @@ while(1):
             else:
                 pass
         else:
+            move(0.0,0.0,0.0)
             print("Stop")
