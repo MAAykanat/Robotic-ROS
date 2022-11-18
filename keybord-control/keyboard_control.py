d4fbc0fbbb7bf41df56cdd0fc4b330fc0abdd9ce
MAAykanat <m.a.aykanat@gmail.com>
2022-11-18 10:30:30 +0300

    Keybord control updated

--- keybord-control/keyboard_control.py
+++ keybord-control/keyboard_control.py
@@ -6,18 +6,21 @@ from nav_msgs import *
 while(1):
     with keyboard.Events() as events:
         # Block for as much as possible
-        event = events.get(1e6)
-        if event.key == keyboard.KeyCode.from_char("w"):
-            print("UP!!!")
-        elif event.key == keyboard.KeyCode.from_char("d"):
-            print("RIGHT!!!")
-        elif event.key == keyboard.KeyCode.from_char("a"):
-            print("LEFT!!!")
-        elif event.key == keyboard.KeyCode.from_char("s"):
-            print("DOWN!!!")
-        elif event.key == keyboard.KeyCode.from_char("p"):
-            print("It is pause!!!")
-        elif event.key == keyboard.KeyCode.from_char("q"):
-            break
+        event = events.get(0.05)
+        if event is not None:
+            if event.key == keyboard.KeyCode.from_char("w"):
+                print("UP!!!")
+            elif event.key == keyboard.KeyCode.from_char("d"):
+                print("RIGHT!!!")
+            elif event.key == keyboard.KeyCode.from_char("a"):
+                print("LEFT!!!")
+            elif event.key == keyboard.KeyCode.from_char("s"):
+                print("DOWN!!!")
+            elif event == None:
+                print("Stop")
+            elif event.key == keyboard.Key.esc:
+                break
+            else:
+                pass
         else:
-            pass
+            print("Stop")
